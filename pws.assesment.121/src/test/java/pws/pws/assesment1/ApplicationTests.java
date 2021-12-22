27eeb7700a40c92ce431809fcc912d148bd3e431
AnggitaTyasmara <63139492+AnggitaTyasmara@users.noreply.github.com>
2021-12-23 05:45:32 +0700

    Add files via upload

---
+++ pws.assesment.121/src/test/java/pws/pws/assesment1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.pws.assesment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
